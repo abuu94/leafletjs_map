cf1e56596d0f5b510e7611fb65c71335b636e83b
Omar Shehe Omar <omar_shehe@yahoo.com>
2020-07-14 23:08:15 +0200

    Update data.php

--- data.php
+++ data.php
@@ -1,46 +1,30 @@
 <?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
    
-   $dbhost = 'localhost';
-   $dbuser = 'root';
-   $dbpass = '';
-   $conn1 = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(!$conn ) {
+    die('Could not connect: ' . mysqli_error($conn));
+}
    
-   if(!$conn1 ) {
-      die('Could not connect: ' . mysqli_error($conn1));
-   }
+$sql = 'SELECT id, name, latitude, longitude FROM worlddata';
+mysqli_select_db(  $conn ,'home_leafletdb' );
+$retval = mysqli_query( $conn, $sql);
+if(!$retval ) {
+    die('Could not retrieve data: ' . mysqli_error($conn));
+}
+$dataArray = array(); 
+while($row = $retval->fetch_assoc()) {
+    array_push($dataArray,array(
+        'id'=>$row['id'],
+        'name'=>$row['name'],
+        'latitude'=>$row['latitude'],
+        'longitude'=>$row['longitude'],
+        )
+    );
+}
+echo json_encode($dataArray);
    
-   //echo 'Connected successfully<br>';
-
-
-    $sql = 'SELECT name FROM worlddata WHERE id=1';
-    mysqli_select_db(  $conn1 ,'home_leafletdb' );
-	 $retval = mysqli_query( $conn1, $sql);
-	 
-	  if(!$retval ) {
-      die('Could not retrieve data: ' . mysqli_error($conn1));
-   }
-   
-   while($row = $retval->fetch_assoc()) {
-      echo "EMP ID :{$row['name']}  <br> ";
-         
-   }
-
-	//function writeMsg() {
-		
-		
-	// $n = "SELECT namr FROM worlddata WHERE id=1 ";
-		
-	
-		// return $n;
-	 
-  
-	//}
-
-	//writeMsg(); 
-	
-	
-    
- 
-   
-   mysqli_close($conn1);
+mysqli_close($conn);
 ?>
